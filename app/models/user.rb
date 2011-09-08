4547136056a14eabbb81f25db9696840052acc8f
Taek MacAir <btaek@withuplus.com>
2011-09-08 16:00:30 +0900

    Added password and encrypted_password attribute to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,27 @@
+class User < ActiveRecord::Base
+  attr_accessor   :password
+  attr_accessible :name, :email, :password, :password_confirmation
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,    :presence     => true,
+                      :length       => { :maximum => 50 }
+  validates :email,   :presence     => true,
+                      :format       => { :with => email_regex },
+                      :uniqueness   => { :case_sensitive => false}
+  validates :password,:presence     => true,
+                      :confirmation => true,
+                      :length       => {:within => 6..40}
+end
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
-class User < ActiveRecord::Base
-  attr_accessible :name, :email
-  
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
-  validates :name,  :presence  => true,
-                    :length   => { :maximum => 50 }
-  validates :email, :presence   => true,
-                    :format     => { :with => email_regex },
-                    :uniqueness => { :case_sensitive => false}
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example user", :email => "user@exampl.com"}
+    @attr = { :name => "Example user", 
+              :email => "user@exampl.com",
+              :password => "foobar",
+              :password_confirmation => "foobar"
+            }
   end
   
   it "should create a new instance given a valid attribute" do
@@ -55,16 +59,66 @@ describe User do
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
+  
+  describe "passwords" do
+    
+    before(:each) do
+      @user = User.new(@attr)
+    end
+    
+    it "should have a password attribute" do
+      @user.should respond_to(:password)
+    end
+    
+    it "should have a password confirmation attribute" do
+      @user.should respond_to(:password_confirmation)
+    end
+  end
+  
+  describe "password validations" do
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confimation => "")).should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short = "a"*5
+      hash = @attr.merge(:password => short, :pasword_confimation => short)
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should reject long passwords" do
+      long = "a"*41
+      hash = @attr.merge(:password => long, :pasword_confimation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+  
+  describe "passwords encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
 end
+
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
