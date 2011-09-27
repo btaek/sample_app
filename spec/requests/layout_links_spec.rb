6faa5525248d5cf96f38b15a7cab0ef94318ece9
Taek <btaek@yahoo.com>
2011-09-27 16:46:23 +0900

    Editing and updating users

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -73,5 +73,10 @@ describe "LayoutLinks" do
       visit root_path
       response.should have_selector("a", :href => user_path(@user), :content => "Profile")
     end
+    
+    it "should have a settgins link" do
+      visit root_path
+      response.should have_selector("a", :href => edit_user_path(@user), :content => "Settings")
+    end
   end
 end
