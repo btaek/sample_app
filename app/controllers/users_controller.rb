888cf77d1fa9823d90b52873c77451a9a8260634
Taek <btaek@yahoo.com>
2011-09-27 19:52:25 +0900

    Working index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate ,:only => [:edit, :update]   #only for edit method, authenticate user signedin
+  before_filter :authenticate ,:only => [:index, :edit, :update]   #only for edit method, authenticate user signedin
   before_filter :correct_user, :only => [:edit, :update]
+  
+  def index
+    @users = User.all
+    @title = "All users"
+  end
   
   def show
     @user = User.find(params[:id])
